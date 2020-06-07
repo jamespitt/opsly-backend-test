189dc018f9709268fb218427c551350f06a5078b
James Pitt <james@ultro.co.uk>
2020-06-07 15:32:42 +0100

    first commmit

---
+++ JamesTest/src/main/java/com/opsly/test/JamesTest/JamesTestApplication.java
@@ -0,0 +1,13 @@
+package com.opsly.test.JamesTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JamesTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JamesTestApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/opsly/test/JamesTest/RestFul/RestFulController.java
@@ -0,0 +1,21 @@
+package com.opsly.test.JamesTest.RestFul;
+
+import org.springframework.http.MediaType;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@EnableAsync
+
+public class RestFulController {
+
+    @RequestMapping(value = "/api", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public String GetData() {
+        return "{}";
+    }
+
+}
