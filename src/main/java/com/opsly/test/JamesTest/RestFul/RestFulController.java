b43076ba2cf5f3a9ef9a8dfca1cf7415b8f4fba2
James Pitt <james@ultro.co.uk>
2020-06-07 15:50:31 +0100

    start to get data inside objects

--- src/main/java/com/opsly/test/JamesTest/RestFul/RestFulController.java
+++ src/main/java/com/opsly/test/JamesTest/RestFul/RestFulController.java
@@ -1,5 +1,10 @@
 package com.opsly.test.JamesTest.RestFul;
 
+import java.util.concurrent.CompletableFuture;
+
+import com.google.gson.Gson;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.scheduling.annotation.EnableAsync;
 
@@ -13,9 +18,26 @@ import org.springframework.web.bind.annotation.RestController;
 
 public class RestFulController {
 
+    @Autowired
+    private SocialClient socialClient;
+
     @RequestMapping(value = "/", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-    public String GetData() {
-        return "{}";
+    public Object GetData() {
+        Object twitter = socialClient.getDataFromUrl("https://takehome.io/twitter");
+        Object facebook = socialClient.getDataFromUrl("https://takehome.io/facebook");
+        Object instragram = socialClient.getDataFromUrl("https://takehome.io/instragram");
+        // CompletableFuture<SocialData> =
+        // socialClient.getDataFromUrl("https://takehome.io/twitter");
+        // CompletableFuture<SocialData> =
+        // socialClient.getDataFromUrl("https://takehome.io/twitter");
+        Object result = new Object();
+        try {
+            result = twitter.get();
+
+        } catch (Throwable e) {
+            //
+        }
+        return result;
     }
 
 }
--- src/main/java/com/opsly/test/JamesTest/RestFul/SocialClient.java
+++ src/main/java/com/opsly/test/JamesTest/RestFul/SocialClient.java
@@ -4,24 +4,54 @@ import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import net.jodah.failsafe.Failsafe;
+import net.jodah.failsafe.RetryPolicy;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
+
+import com.fasterxml.jackson.core.io.JsonEOFException;
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 
 @Service
 public class SocialClient {
     RestTemplate restTemplate = new RestTemplate();
+    static final int MAX_RETRIES = 10;
 
-    @Async
-    public CompletableFuture<SocialData> getDataFromUrl(String url) {
-        SocialData response;
+    private static RetryPolicy retryPolicy = new RetryPolicy().withMaxRetries(MAX_RETRIES);
+
+    public JsonArray getDataFromUrl(String url) {
+        // Gson gson = new Gson();
 
         // try {
-        response = restTemplate.getForObject(url, SocialData.class);
+
+        return Failsafe.with(retryPolicy).get(() -> restTemplate.getForObject(url, JsonArray.class));
         // } catch (Exception e) {
         //
 
         // }
 
-        return CompletableFuture.completedFuture(response);
     }
+    
+
+    public List<String> getListFromUrl(String url, String type) {
+        List<String> list = new ArrayList<String>();
+
+        JsonArray data = getDataFromUrl(url);
+        for (JsonElement datapointElement: data) {
+            JsonObject datapointObject = datapointElement.getAsJsonObject(); 
+            String dataToAdd = datapointObject.get(type).getAsString();
+            list.add(dataToAdd);
+
+        }
+
+        return list;
+    }
+
 
 }
